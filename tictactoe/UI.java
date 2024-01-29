b22616c7df0f164b375b6db6c9433592e3523c04
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2024-01-29 11:48:21 -0800

    Debugging MakeAMove

--- tictactoe/UI.java
+++ tictactoe/UI.java
@@ -15,27 +15,33 @@ public class UI
     
      // Utility methods
     public String getXOrO(int whoseMove) {
-        return (whoseMove == -1) ? "X" : "O";
+        if (whoseMove == -1) {
+            return "X";
+        } else if (whoseMove == 1) {
+            return "O";
+        } else {
+            return " ";
+        }
     }
 
     public String getPlayerName(int whoseMove, String xName, String oName) {
         return (whoseMove == -1) ? xName : oName;
     }
 
-    public boolean isLegalMove(int state, int row, int col) {
+    public boolean isLegalMove(State state, int row, int col) {
         return 1 <= row && row <= Constants.BOARD_SIZE &&
         1 <= col && col <= Constants.BOARD_SIZE &&
         state.getBoardCell(row, col) == Constants.BLANK;
     }
 
     // Prompt for input methods
-    public String promptForName(int player) {
+    public String promptForName(String player) {
         System.out.printf(Constants.GET_PLAYER_NAME, player);
         return scanner.next();
     }
 
     public int getMoveRow(int whoseMove, String xName, String oName) {
-        int row;
+        int row = 0;
         while (row <= 0 || row >= 4) {
             try {
                 System.out.printf(Constants.GET_ROW_MOVE, getXOrO(whoseMove), getPlayerName(whoseMove, xName, oName));
@@ -48,7 +54,7 @@ public class UI
     }
 
     public int getMoveCol(int whoseMove, String xName, String oName) {
-        int col;
+        int col = 0;
         while (col <= 0 || col >= 4) {
             try {
                 System.out.printf(Constants.GET_COL_MOVE, getXOrO(whoseMove), getPlayerName(whoseMove, xName, oName));
@@ -75,6 +81,7 @@ public class UI
         System.out.println(Constants.DIVIDER_STRING);
         for (int row = 0; row < Constants.BOARD_SIZE; row++) {
             System.out.printf(Constants.BOARD_STRING, getXOrO(state.getBoardCell(row, 0)), getXOrO(state.getBoardCell(row, 1)), getXOrO(state.getBoardCell(row, 2)));
+            System.out.println();
             System.out.println(Constants.DIVIDER_STRING);
         }
     }
