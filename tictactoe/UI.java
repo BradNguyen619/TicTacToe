e3dc5ac4af3710308275d794787a4979fc394c3f
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2024-01-31 11:39:53 -0800

    NewGamePromptBug 5:00

--- tictactoe/UI.java
+++ tictactoe/UI.java
@@ -31,7 +31,7 @@ public class UI
     public boolean isLegalMove(State state, int row, int col) {
         return 1 <= row && row <= Constants.BOARD_SIZE &&
         1 <= col && col <= Constants.BOARD_SIZE &&
-        state.getBoardCell(row, col) == Constants.BLANK;
+        state.getBoardCell(row-1, col-1) == Constants.BLANK;
     }
 
     // Prompt for input methods
@@ -69,7 +69,7 @@ public class UI
     public boolean startNewGame() {
         System.out.println(Constants.START_NEW_GAME);
         String yesOrNo = scanner.next();
-        return yesOrNo == "T";
+        return yesOrNo.equals("Y") || yesOrNo.equals("y");
     }
 
     // Printing text methods
@@ -101,6 +101,7 @@ public class UI
 
     public void printWinner(State state) {
         System.out.printf(Constants.WINNER, getXOrO(state.getWhoseMove()), getPlayerName(state.getWhoseMove(), state.getXName(), state.getOName()));
+        System.out.println();
     }
 
     public void printTieGame() {
